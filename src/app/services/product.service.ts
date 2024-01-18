0ee58fb6c1ae0813c211ccf6dbef6c8aaa05149d
Max Hart <maxhart5000@icloud.com>
2024-01-18 19:12:13 +0800

    Initial commit. Creating a frontend with angular and typescript to connect with my backend spring boot application. Creating a product class that coincides with the product entities stored on the db. Creating a product-list.component that presents all of the retrieved products from the db to the client-side. Makes use of a product.service that helps convert the incoming data from the backend to the useable product variable. Adding functionality to the frontend such that you can search by product category by clicking the corresponding category link.

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(categoryId: number): Observable<Product[]> {
+
+    // Need to build url based on category id 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      map(response => response._embedded.products));
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
